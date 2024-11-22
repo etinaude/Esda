8c645e32f4a4032e339b06834984081d54104dd6
Etienne Naude <eti@naude.dev>
2024-11-22 13:53:37 +0000

    valve to platformio

--- valve/src/main.cpp
+++ valve/src/main.cpp
@@ -1,43 +1,21 @@
-#include <WiFi.h>
 #include <ArduinoWebsockets.h>
+#include <Arduino.h>
 #include <Arduino_JSON.h>
+#include <WiFi.h>
 #include "FastLED.h"
+
+#define valvePinA 5
+#define valvePinB 6
+#define LEDPin 7
 
 using namespace websockets;
 
-// LED and valve pin definitions
-#define NumPixels 1
-#define LEDPin 11
-#define valvePinA 9
-#define valvePinB 10
-
-CHSV leds[NumPixels];
-
-// WiFi and WebSocket configuration
-const char* ssid = "Etienne";
-const char* password = "etienne123";
+String ssid = "Etienne";
+String password = "etienne123";
 String deviceID = "cf414906-b98b-4c42-bcac-c64987501bd9";
-String wsServer = "ws://192.168.176.96:3000"; // WebSocket server address
+String wsServer = "ws://192.168.176.96:3000";
 
 WebsocketsClient wsClient;
-
-void setup() {
-  Serial.begin(9600);
-  setupPins();
-  setupWifi();
-  connectWebSocket();
-}
-
-void loop() {
-  // Handle WebSocket events
-  wsClient.poll();
-}
-
-void setupPins() {
-  pinMode(LEDPin, OUTPUT);
-  pinMode(valvePinA, OUTPUT);
-  pinMode(valvePinB, OUTPUT);
-}
 
 void setupWifi() {
   WiFi.begin(ssid, password);
@@ -52,11 +30,47 @@ void setupWifi() {
   Serial.println(WiFi.localIP());
 }
 
+void setupPins() {
+  pinMode(LEDPin, OUTPUT);
+  pinMode(valvePinA, OUTPUT);
+  pinMode(valvePinB, OUTPUT);
+}
+
+void handleValve(bool on) {
+  int pin = on ? valvePinA : valvePinB;
+
+  digitalWrite(pin, HIGH);
+  delay(500);
+  digitalWrite(pin, LOW);
+}
+
+void onWebSocketMessage(WebsocketsMessage message) {
+  Serial.println("Message received: ");
+  Serial.println(message.data());
+
+  JSONVar json = JSON.parse(message.data());
+  if (JSON.typeof(json) == "undefined") {
+    Serial.println("Invalid JSON received");
+    return;
+  }
+
+  if (json["deviceId"] == deviceID) {
+    int pose = (int)json["pose"];
+    String event = (const char*)json["event"];
+
+    Serial.print("Pose: ");
+    Serial.println(pose);
+
+    if (pose >= 0 && pose < 7) handleValve(true);
+    if (event == "cancel") handleValve(false);
+  } else {
+    Serial.println("Message not intended for this device.");
+  }
+}
+
 void connectWebSocket() {
-  // Connect to WebSocket server
   Serial.println("Connecting to WebSocket server...");
-  wsClient.onMessage(onWebSocketMessage); // Set callback for received messages
-  wsClient.onEvent(onWebSocketEvent);     // Set callback for WebSocket events
+  wsClient.onMessage(onWebSocketMessage);
   wsClient.connect(wsServer);
 
   if (wsClient.available()) {
@@ -66,83 +80,17 @@ void connectWebSocket() {
   }
 }
 
-void onWebSocketMessage(WebsocketsMessage message) {
-  // Handle received messages
-  Serial.println("Message received: ");
-  Serial.println(message.data());
-
-  // Parse the received message (assuming JSON format)
-  JSONVar json = JSON.parse(message.data());
-  if (JSON.typeof(json) == "undefined") {
-    Serial.println("Invalid JSON received");
-    return;
-  }
-
-  // Check if the message is for this device
-  if (String(json["deviceId"]) == deviceID) {
-
-    // Check if the message is concerning a potential seizure pose that has been detected
-    int pose = (const int)json["pose"];
-    Serial.print("Pose: ");
-    Serial.println(pose);
-
-    if (pose >= 0 && pose < 7) {
-      handleMotor(true); // Turn motor ON
-    }
-
-    // String status = (const char*)json["status"];
-    // Serial.print("Status: ");
-    // Serial.println(status);
-
-    // if (status == "ON") {
-      
-    // } else if (status == "OFF") {
-    //   handleMotor(false); // Turn motor OFF
-    // } else if (status == "LowBattery") {
-    //   Serial.println("Low battery warning!");
-    //   handleLowBattery();
-    // }
-  } else {
-    Serial.println("Message not intended for this device.");
-  }
+void setup() {
+  Serial.begin(9600);
+  setupPins();
+  setupWifi();
+  connectWebSocket();
 }
 
-void onWebSocketEvent(WebsocketsEvent event, String data) {
-  switch (event) {
-    case WebsocketsEvent::ConnectionOpened:
-      Serial.println("WebSocket connection opened.");
-      break;
-    case WebsocketsEvent::ConnectionClosed:
-      Serial.println("WebSocket connection closed.");
-      break;
-    case WebsocketsEvent::GotPing:
-      Serial.println("WebSocket Ping!");
-      break;
-    case WebsocketsEvent::GotPong:
-      Serial.println("WebSocket Pong!");
-      break;
+void loop() {
+  if (!wsClient.available()) {
+    Serial.println("WebSocket disconnected. Reconnecting...");
+    connectWebSocket();
   }
-}
-
-void handleMotor(bool onOff) {
-  if (onOff) {
-    digitalWrite(valvePinA, HIGH);
-    digitalWrite(valvePinB, HIGH);
-    Serial.println("Motor turned ON");
-  } else {
-    digitalWrite(valvePinA, LOW);
-    digitalWrite(valvePinB, LOW);
-    Serial.println("Motor turned OFF");
-  }
-}
-
-void handleLowBattery() {
-  // Flash LED to indicate low battery
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(LEDPin, HIGH);
-    delay(250);
-    digitalWrite(LEDPin, LOW);
-    delay(250);
-  }
-  Serial.println("Low battery LED warning completed.");
+  wsClient.poll();
 }
