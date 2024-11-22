ae28ab1481bf3888aa7313d0100c1c2bbc805590
Etienne Naude <eti@naude.dev>
2024-11-22 13:54:31 +0000

    add door code

---
+++ door/src/main.cpp
@@ -0,0 +1,96 @@
+#include <ArduinoWebsockets.h>
+#include <Arduino.h>
+#include <Arduino_JSON.h>
+#include <WiFi.h>
+#include "FastLED.h"
+
+#define valvePinA 5
+#define valvePinB 6
+#define LEDPin 7
+
+using namespace websockets;
+
+String ssid = "Etienne";
+String password = "etienne123";
+String deviceID = "cf414906-b98b-4c42-bcac-c64987501bd9";
+String wsServer = "ws://192.168.176.96:3000";
+
+WebsocketsClient wsClient;
+
+void setupWifi() {
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setupPins() {
+  pinMode(LEDPin, OUTPUT);
+  pinMode(valvePinA, OUTPUT);
+  pinMode(valvePinB, OUTPUT);
+}
+
+void handleValve(bool on) {
+  int pin = on ? valvePinA : valvePinB;
+
+  digitalWrite(pin, HIGH);
+  delay(500);
+  digitalWrite(pin, LOW);
+}
+
+void onWebSocketMessage(WebsocketsMessage message) {
+  Serial.println("Message received: ");
+  Serial.println(message.data());
+
+  JSONVar json = JSON.parse(message.data());
+  if (JSON.typeof(json) == "undefined") {
+    Serial.println("Invalid JSON received");
+    return;
+  }
+
+  if (json["deviceId"] == deviceID) {
+    int pose = (int)json["pose"];
+    String event = (const char*)json["event"];
+
+    Serial.print("Pose: ");
+    Serial.println(pose);
+
+    if (pose >= 0 && pose < 7) handleValve(true);
+    if (event == "cancel") handleValve(false);
+  } else {
+    Serial.println("Message not intended for this device.");
+  }
+}
+
+void connectWebSocket() {
+  Serial.println("Connecting to WebSocket server...");
+  wsClient.onMessage(onWebSocketMessage);
+  wsClient.connect(wsServer);
+
+  if (wsClient.available()) {
+    Serial.println("WebSocket connected!");
+  } else {
+    Serial.println("WebSocket connection failed.");
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  setupPins();
+  setupWifi();
+  connectWebSocket();
+}
+
+void loop() {
+  if (!wsClient.available()) {
+    Serial.println("WebSocket disconnected. Reconnecting...");
+    connectWebSocket();
+  }
+  wsClient.poll();
+}
